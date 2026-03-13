cf795a90166898d2b0bf9f137ac63437327c3088
jabukufo <collinmckinney4@gmail.com>
2026-03-13 08:24:00 -0500

    Cleaned up some warnings.

--- src/main/java/com/ScapeSociety/ScapeSocietyPlugin.java
+++ src/main/java/com/ScapeSociety/ScapeSocietyPlugin.java
@@ -5,12 +5,8 @@ import javax.inject.Inject;
 import javax.swing.*;
 
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
@@ -25,11 +21,6 @@ import java.awt.image.BufferedImage;
 )
 public class ScapeSocietyPlugin extends Plugin
 {
-	@Inject
-	private Client client;
-
-	@Inject
-	private ScapeSocietyConfig config;
 
 	@Inject
 	private ClientToolbar clientToolbar;
